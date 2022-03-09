3d81a27142cd077e0e04e8fbbf827f7de644f287
Marius Kristiansen <marius.kristiansen@ikomm.no>
2022-03-09 14:31:15 +0100

    init

---
+++ ApiUrls.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiDemo
+{
+    public static class ApiUrls
+    {
+        public const string BaseUrl = "https://api.punkapi.com/v2/";
+        public static string Beers => BaseUrl + "beers";
+        public static string Beer(long index) => Beers + $"/{index}";
+    }
+}
